704f92cd057f4e893f3b6d9874b755a5fb3ffa1b
Damien Churchill <damien.churchill@ukplc.net>
2011-08-30 14:06:32 +0100

    examples: improve rados_fetch_file

--- examples/rados_fetch_file.py
+++ examples/rados_fetch_file.py
@@ -2,9 +2,13 @@ from ceph.rados import Rados
 
 rados = Rados(conf_file='/etc/ceph/ceph.conf')
 
+if 'example' not in rados.pools:
+    raise Exception('Missing pool')
+
 pool = rados.pools['example']
 for line in pool.open('foo'):
     print line
 
+# not really required as it's the end of the script
 pool.close()
 rados.shutdown()
